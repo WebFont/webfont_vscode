8f0ba0fb795893f68e9edddfb862a8479ed97657
alext <softlink@bk.ru>
2022-08-14 15:32:31 +0700

    New api, ux improvements

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,7 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
 
-const FONT_LIST_URL = 'https://fontstorage.com/api/list.json';
-
+const FONT_LIST_URL = 'https://fontstorage.com/api/plugins.json';
 class UiFontItem {
     label: string;
     fontObject: any;
@@ -18,36 +17,41 @@ export function activate(context: vscode.ExtensionContext) {
 
     console.log('Congratulations, your extension "webfont-vscode" is now active!');
 
-    async function downloadFontsList(): Promise<Array<any>> {
-        let fontsList = context.globalState.get('webfontCache', []);
-
-        if (fontsList.length > 0) {
-            return fontsList;
-        } else {
-            try {
-                const { data } = await axios.get(FONT_LIST_URL);
-                context.globalState.update('webfontCache', data);
-                return data;
-            }
-            catch (err) {
-                return [{ name: "Can't download font's list. Check if you connected to the Internet." }];
-            }
+    async function downloadFontsList(): Promise<any> {
+        try {
+            const { data } = await axios.get(FONT_LIST_URL);
+            
+            context.globalState.update('webfontCache', data);
+            return data;
         }
+        catch (err) {
+            return [{ name: "Can't download font's list. Check if you connected to the Internet." }];
+        }
+
+    }
+
+    async function getUrls() {
+        const data = await downloadFontsList();
+        return data.urls;
     }
 
     async function prepareUIFontList() {
-        const fontsList = await downloadFontsList();
+        const data = await downloadFontsList();
+        const fontsList = data.fonts;        
         const uiFontsList: UiFontItem[] = [];
 
-        fontsList.forEach(element => {
+        fontsList.forEach((element:any) => {
             uiFontsList.push(new UiFontItem(element.name, element));
         });
 
         return uiFontsList;
     }
 
-    let importFontCommand = vscode.commands.registerCommand('webfont.importFont', async () => {
-        const uiFontsList = await prepareUIFontList();
+    let fontCommand = vscode.commands.registerCommand('webfont', async () => {
+        const uiFontsList = await prepareUIFontList();   
+        const urls = await getUrls();    
+        const menuItems = ['Import font', 'Download font(ttf/otf)', 'Subsetting', 'View on website'];
+        
         const selectedFont = await vscode.window.showQuickPick(uiFontsList, { placeHolder: 'Select the font' });
 
         if (selectedFont) {
@@ -56,33 +60,41 @@ export function activate(context: vscode.ExtensionContext) {
                 return; // No open text editor
             }
 
-            editor.edit((builder) => {
-                if (editor) {
-                    let textToInsert = `/* Please do not use this import in production. You could download this font from here ${selectedFont.fontObject.font_url} */`;
-                    textToInsert += '\n';
-                    textToInsert += selectedFont.fontObject.import;
-                    textToInsert += '\n';
-                    textToInsert += selectedFont.fontObject.comments;
+            const linkToSite = `${urls.site_url}/font/${selectedFont.fontObject.font_slug}`;
 
-                    builder.insert(editor.selection.active, textToInsert);
-                    vscode.window.showInformationMessage('Font import added.');
-                }
-            });
+            const selectedOption = await vscode.window.showQuickPick(menuItems, {title: 'Choose option', canPickMany: false});
+
+            if (selectedOption === 'Import font') {
+                editor.edit((builder) => {
+                    if (editor) {
+                        let textToInsert = `/* Please do not use this import in production. You could download this font from here ${linkToSite} */`;
+                        textToInsert += '\n';
+                        textToInsert += `@import "${urls.import_url}${selectedFont.fontObject.slug}.css";`;
+                        textToInsert += '\n';
+                        textToInsert += selectedFont.fontObject.comments;
+
+                        builder.insert(editor.selection.active, textToInsert);
+                        vscode.window.showInformationMessage('Font import added.');
+                    }
+                });
+            }
+
+            if (selectedOption === 'Download font(ttf/otf)') {
+                vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${urls.download_url}${selectedFont.fontObject.font_slug}/${selectedFont.fontObject.font_slug}.zip`))
+            }
+
+            if (selectedOption === 'Subsetting') {
+                vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${urls.converter_url}#${selectedFont.fontObject.font_slug}`));
+            }
+
+            if (selectedOption === 'View on website') {
+                vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${linkToSite}?from=vscode`));
+            }
+
         }
     });
 
-    context.subscriptions.push(importFontCommand);
-
-    let downloadFontCommand = vscode.commands.registerCommand('webfont.downloadFont', async () => {
-        const uiFontsList = await prepareUIFontList();
-        const selectedFont = await vscode.window.showQuickPick(uiFontsList, { placeHolder: 'Select the font' });
-
-        if (selectedFont) {
-            vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(selectedFont.fontObject.font_url + "?utm_source=vscode"));
-        }
-    });
-
-    context.subscriptions.push(downloadFontCommand);
+    context.subscriptions.push(fontCommand);
 }
 
 // this method is called when your extension is deactivated
